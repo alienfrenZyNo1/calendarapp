8a3b7999dbf09a411ece78d11700f6aa7f9b809a
adrianmarikar <adrianmarikar@gmail.com>
2024-04-17 11:57:30 +0100

    Making 1000 random calendar entries for testing

--- lib/data/repositories/calendar_repository.dart
+++ lib/data/repositories/calendar_repository.dart
@@ -1,25 +1,50 @@
 // data/repositories/calendar_repository.dart
+import 'package:faker/faker.dart';
 import 'package:stpeters_calendar_app/models/calendar_event_model.dart';
 
 import '../../utils/logger.dart';
 
 class MockCalendarRepository implements CalendarRepository {
-  final List<CalendarEventModel> _events = [
-    CalendarEventModel(
-      id: "event_1",
-      title: "Staff Meeting",
-      description: "Monthly staff meeting to discuss school progress",
-      startDate: DateTime(2024, 04, 25, 10, 0), // Adjust date/time as needed
-      endDate: DateTime(2024, 04, 25, 11, 0), // Adjust date/time as needed
-    ),
-    CalendarEventModel(
-      id: "event_2",
-      title: "Science Fair",
-      description: "Annual science fair showcasing student projects",
-      startDate: DateTime(2024, 05, 10, 14, 0), // Adjust date/time as needed
-      endDate: DateTime(2024, 05, 10, 17, 0), // Adjust date/time as needed
-    ),
-  ];
+  // final List<CalendarEventModel> _events = [
+  //   CalendarEventModel(
+  //     id: "event_1",
+  //     title: "Staff Meeting",
+  //     description: "Monthly staff meeting to discuss school progress",
+  //     startDate: DateTime(2024, 04, 25, 10, 0), // Adjust date/time as needed
+  //     endDate: DateTime(2024, 04, 25, 11, 0), // Adjust date/time as needed
+  //   ),
+  //   CalendarEventModel(
+  //     id: "event_2",
+  //     title: "Science Fair",
+  //     description: "Annual science fair showcasing student projects",
+  //     startDate: DateTime(2024, 05, 10, 14, 0), // Adjust date/time as needed
+  //     endDate: DateTime(2024, 05, 10, 17, 0), // Adjust date/time as needed
+  //   ),
+  //   CalendarEventModel(
+  //     id: "event_3",
+  //     title: "Marty Party",
+  //     description: "Annual marty party",
+  //     startDate: DateTime(2024, 05, 10, 14, 0), // Adjust date/time as needed
+  //     endDate: DateTime(2024, 05, 10, 17, 0), // Adjust date/time as needed
+  //   ),
+  // ];
+
+  final List<CalendarEventModel> _events = List.generate(1000, (index) {
+    final faker = Faker();
+    final randomStartDays = faker.randomGenerator
+        .integer(15); // Generate random number of days within 15 days
+    final randomEndHours = faker.randomGenerator
+        .integer(24); // Generate random number of hours within 24 hours
+    final startDate = DateTime.now().add(Duration(days: randomStartDays));
+    final endDate = startDate.add(Duration(hours: randomEndHours));
+    return CalendarEventModel(
+      id: "event_${index + 1}",
+      title: faker.company.name(),
+      description: faker.lorem.sentence(),
+      startDate: startDate,
+      endDate: endDate,
+    );
+  });
 
   @override
   Future<List<CalendarEventModel>> getEvents() async {
--- lib/ui/widgets/schedule_widget.dart
+++ lib/ui/widgets/schedule_widget.dart
@@ -1,66 +1,98 @@
-// ui/widgets/schedule_widget.dart
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart'; // Import DateFormat
+import '../../bloc/calendar/calendar_bloc.dart';
+import '../../models/calendar_event_model.dart'; // Import CalendarEventModel
 
 class ScheduleWidget extends StatelessWidget {
   const ScheduleWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Mock list of events for testing
-    List<Event> events = [
-      Event(
-          title: 'Meeting',
-          startTime: const TimeOfDay(hour: 9, minute: 0),
-          endTime: const TimeOfDay(hour: 10, minute: 0)),
-      Event(
-          title: 'Lunch',
-          startTime: const TimeOfDay(hour: 12, minute: 0),
-          endTime: const TimeOfDay(hour: 13, minute: 0)),
-      Event(
-          title: 'Presentation',
-          startTime: const TimeOfDay(hour: 14, minute: 0),
-          endTime: const TimeOfDay(hour: 15, minute: 0)),
-      Event(
-          title: 'Training',
-          startTime: const TimeOfDay(hour: 16, minute: 0),
-          endTime: const TimeOfDay(hour: 17, minute: 0)),
-    ];
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        leading: IconButton(
-          icon: const Icon(Icons.menu),
-          onPressed: () {
-            // Open drawer
-            Scaffold.of(context).openDrawer();
-          },
-        ),
-        title: const Text('Schedule'),
-      ),
-      body: ListView.builder(
-        itemCount: events.length,
-        itemBuilder: (context, index) {
-          final event = events[index];
-          return ListTile(
-            title: Text(event.title),
-            subtitle: Text(
-                '${event.startTime.format(context)} - ${event.endTime.format(context)}'),
-            onTap: () {
-              // Add functionality to handle tapping on events
-            },
+    return BlocBuilder<CalendarBloc, CalendarState>(
+      builder: (context, state) {
+        if (state is LoadedState) {
+          // Check if events are loaded successfully
+          final events = state.events;
+          // Group events by date
+          final groupedEvents = _groupEventsByDate(events);
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.blue,
+              leading: IconButton(
+                icon: const Icon(Icons.menu),
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
+                },
+              ),
+              title: const Text('Schedule'),
+            ),
+            body: ListView.builder(
+              itemCount: groupedEvents.length,
+              itemBuilder: (context, index) {
+                final date = groupedEvents.keys.toList()[index];
+                final dateEvents = groupedEvents[date]!;
+                return _buildDateSection(date, dateEvents);
+              },
+            ),
           );
-        },
-      ),
+        } else {
+          // If events are not loaded yet, show a loading indicator
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
     );
   }
-}
 
-class Event {
-  final String title;
-  final TimeOfDay startTime;
-  final TimeOfDay endTime;
+  // Helper function to group events by date
+  Map<DateTime, List<CalendarEventModel>> _groupEventsByDate(
+      List<CalendarEventModel> events) {
+    final groupedEvents = <DateTime, List<CalendarEventModel>>{};
+    for (final event in events) {
+      final date = DateTime(
+          event.startDate.year, event.startDate.month, event.startDate.day);
+      if (!groupedEvents.containsKey(date)) {
+        groupedEvents[date] = [];
+      }
+      groupedEvents[date]!.add(event);
+    }
+    return groupedEvents;
+  }
 
-  Event({required this.title, required this.startTime, required this.endTime});
+  // Helper function to build a section for a specific date
+  Widget _buildDateSection(DateTime date, List<CalendarEventModel> events) {
+    final formattedDate = DateFormat.yMMMMd().format(date);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+          child: Text(
+            formattedDate,
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+        ),
+        ListView.builder(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: events.length,
+          itemBuilder: (context, index) {
+            final event = events[index];
+            final startTimeFormatted = DateFormat.Hm().format(event.startDate);
+            final endTimeFormatted = DateFormat.Hm().format(event.endDate);
+            return ListTile(
+              title: Text(event.title),
+              subtitle: Text('$startTimeFormatted - $endTimeFormatted'),
+              onTap: () {
+                // Add functionality to handle tapping on events
+              },
+            );
+          },
+        ),
+        const Divider(),
+      ],
+    );
+  }
 }
